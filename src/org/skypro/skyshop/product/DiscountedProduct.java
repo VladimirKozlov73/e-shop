9865c4fd8c005194640e3c6fefe2976c07c36599
VladimirKozlov73 <postwsz@mail.ru>
2025-06-08 22:34:17 +0300

    Создан класс DiscountedProduct наследник класса Product

---
+++ src/org/skypro/skyshop/product/DiscountedProduct.java
@@ -0,0 +1,19 @@
+package org.skypro.skyshop.product;
+
+public class DiscountedProduct extends Product{
+
+    protected int basePrice;
+    protected int discount;
+
+    public DiscountedProduct(String name, int basePrice, int discount) {
+        super(name);
+        this.basePrice = basePrice;
+        this.discount = discount;
+    }
+
+    public int getPrice() {
+        int price = 0;
+        price = (int) Math.round (basePrice * (1 - discount / 100.0));
+        return price;
+    }
+}
